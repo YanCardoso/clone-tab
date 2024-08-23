df09eaa12e51cb654c56e8ac39a8338f9691e6e6
Yan Cardoso <yancard7@gmail.com>
2024-08-23 16:08:59 -0300

    compose config

--- pages/api/v1/status.js
+++
@@ -1,5 +0,0 @@
-function status(request, response) {
-  response.status(200).json({ chave: "teste de api" });
-}
-
-export default status;
---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,10 @@
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 10 + 10 as sum");
+  console.log(result);
+
+  response.status(200).json({ chave: "teste de api" });
+}
+
+export default status;
