6c85a52a9cb00e31a045168f16ddfe4b5f2628c0
Yan Cardoso <yancard7@gmail.com>
2025-03-01 18:13:05 -0300

    Completes the `/api/v1/status` endpoint.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,20 +2,32 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const serveStatus = await database.query(
-    `SELECT 
-      current_setting('server_version') AS server_version,
-      current_setting('max_connections') AS max_connections,
-      (SELECT count(*) FROM pg_stat_activity WHERE state = 'active') AS current_connections;
-    `,
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
   );
+  const databaseMaxConnectionsValue = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
-    data_infos: {
-      version: serveStatus.rows[0]["server_version"],
-      max_connections: parseInt(serveStatus.rows[0]["max_connections"]),
-      current_connections: serveStatus.rows[0]["current_connections"],
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
     },
   });
 }
--- tests/integrad/v1/status/get.test.js
+++ tests/integrad/v1/status/get.test.js
@@ -4,10 +4,10 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const responseBody = await response.json();
 
-  console.log(responseBody.data_infos);
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
-  expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.data_infos["version"]).toBeDefined();
-  expect(responseBody.data_infos["max_connections"]).toBeDefined();
-  expect(responseBody.data_infos["current_connections"]).toBeDefined();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
