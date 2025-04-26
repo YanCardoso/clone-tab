3b0d00b073bc07ba0319016e408ce0750402f39b
Yan Cardoso <yancard7@gmail.com>
2025-04-26 19:03:32 -0300

    refactor: consolidate migration options and improve test assertions

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,30 +1,30 @@
 import migrationsRunner from 'node-pg-migrate';
 import {join} from 'node:path';
 export default async function migrations(request, response) {
+  const defaultMigrationsOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join('infra', 'migrations'),
+    direction: 'up',
+    verbose: true,
+    migrationsTable: 'pgmigrations'
+  };
   if (request.method === 'GET') {
-    const migrations = await migrationsRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join('infra', 'migrations'),
-      direction: 'up',
-      verbose: true,
-      migrationsTable: 'pgmigrations'
-    });
-
-    return response.status(200).json(migrations);
+    const pendingMigrations = await migrationsRunner(defaultMigrationsOptions);
+    return response.status(200).json(pendingMigrations);
   }
 
   if (request.method === 'POST') {
-    const migrations = await migrationsRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: false,
-      dir: join('infra', 'migrations'),
-      direction: 'up',
-      verbose: true,
-      migrationsTable: 'pgmigrations'
+    const migratedMigrations = await migrationsRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false
     });
 
-    return response.status(200).json(migrations);
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
   }
 
   return response.status(405).end();
--- tests/integration/v1/migrations/post.test.js
+++ tests/integration/v1/migrations/post.test.js
@@ -7,12 +7,25 @@ async function cleanDatabase() {
 
 beforeAll(cleanDatabase);
 test('POST to /api/v1/migrations should return 200', async () => {
-  const response = await fetch('http://localhost:3000/api/v1/migrations', {
+  const response1 = await fetch('http://localhost:3000/api/v1/migrations', {
     method: 'POST'
   });
-  expect(response.status).toBe(200);
+  expect(response1.status).toBe(201);
 
-  const responseBody = await response.json();
+  const responseBody = await response1.json();
 
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
+
+test('POST to /api/v1/migrations should return 200', async () => {
+  const response2 = await fetch('http://localhost:3000/api/v1/migrations', {
+    method: 'POST'
+  });
+  expect(response2.status).toBe(200);
+
+  const responseBody = await response2.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBe(0);
 });
